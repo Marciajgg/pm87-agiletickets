052aa1b4ecd49c2745eccfb52c1fe4b061a52406
Bruno <soujava@gmail.com>
2017-02-08 17:57:08 -0200

    Refatoração 1

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -2,11 +2,7 @@ package br.com.caelum.agiletickets.domain.precos;
 
 import java.math.BigDecimal;
 
-import br.com.caelum.agiletickets.models.BalletOrquestra;
-import br.com.caelum.agiletickets.models.CinemaShow;
 import br.com.caelum.agiletickets.models.Espetaculo;
-import br.com.caelum.agiletickets.models.OutrosEventos;
-import br.com.caelum.agiletickets.models.PrecoIngresso;
 import br.com.caelum.agiletickets.models.Sessao;
 import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 
@@ -14,21 +10,9 @@ public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
-		PrecoIngresso ingresso;
 		Espetaculo espetaculo = sessao.getEspetaculo();
 		TipoDeEspetaculo tipo = espetaculo.getTipo();
-		
-		if(tipo.equals(TipoDeEspetaculo.CINEMA) || tipo.equals(TipoDeEspetaculo.SHOW)) {
-			ingresso = new CinemaShow(); 
-			preco = ingresso.calculaPreco(sessao);
-		} else if(tipo.equals(TipoDeEspetaculo.BALLET) || tipo.equals(TipoDeEspetaculo.ORQUESTRA)) {
-			ingresso = new BalletOrquestra();
-			preco = ingresso.calculaPreco(sessao);
-		}  else {
-			ingresso = new OutrosEventos();
-			preco = ingresso.calculaPreco(sessao);
-		} 
-
+		preco = tipo.getPreco().calculaPreco(sessao);
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
 
